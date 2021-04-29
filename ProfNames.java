6f9e32f555baefcc8c12f87d87c560e673b2498d
Wesley Truong <>
2021-04-29 06:27:47 +0000

    Pasted

--- ProfNames.java
+++ ProfNames.java
@@ -2,8 +2,13 @@ public class ProfNames
 {
   public int countNames(String[][]nameArray, String lastName)
   {
-
-
-    return 0;
+    int count = 0;
+for (int r = 0; r < nameArray.length; r++) {
+      for (int c = 0; c < nameArray[0].length; c++){
+           if (nameArray[r][c].substring(0,nameArray[r][c].indexOf(",")).equals(lastName) )
+               count++;
+      }
+}
+return count;
   }
 }
